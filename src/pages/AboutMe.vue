6416a168165de1920ec9d291cdd4181033a1ce33
6210110086 <6210110086@psu.ac.th>
2022-09-17 00:19:38 +0700

    project

---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,26 @@
+<template>
+  <img class="full-screen"
+    src="https://img.pikbest.com/background/20180830/seaside-playing-cartoon-cute-background-design_2746045.jpg!c1024wm0">
+  <div class="absolute-top text-center">
+     <br />
+    <br />
+    <h2>ฝึกอ่านออกเสียง</h2>
+  </div>
+  <div class="fixed-center full-width">
+    <div class="space-between">
+
+      <audio controls>
+        <source src="~assets/audios/เสียงข.mp3" type="audio/mpeg">
+      </audio>
+      <img
+        src="~assets/images/ข.jpg">
+
+      <img src="~assets/images/ไข่ไก่.png">
+    </div>
+
+  </div>
+
+  <div class="q-gutter-md row">
+      <q-btn color="primary"  class="absolute-bottom-right" label="BACK" @click="$router.replace('/')"/></div>
+
+</template>
